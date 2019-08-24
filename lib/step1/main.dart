451a83779534ae5d686a7370d0bf3c444977aa96
Scott <scottstoll2017@gmail.com>
2019-08-24 14:56:22 -0400

    Step 1 Written... could use more doc comments

--- lib/step1/main.dart
+++ lib/step1/main.dart
@@ -6,11 +6,11 @@ class ImageStep extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Breaking it Down',
+      title: 'Rudimentary Layouts',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(title: 'Breaking it Down'),
+      home: Home(title: 'Rudimentary Layouts'),
     );
   }
 }
@@ -41,7 +41,53 @@ class PageContent extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
 
+            /// You need an expanded when nesting rows and columns or thing blow up.
+            /// "Unbounded" size limits (called "Constraints")
+            Expanded(
+              child: Container(
+                height: 100,
+
+                /// No width needed since the expanded will eat up everything left over.
+                /// Even if we did put a width here, it would be ignored.
+                color: Colors.green,
+              ),
+            ),
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width / 3,
+              color: Colors.purple,
+            ),
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width / 3,
+              color: Colors.red,
+            ),
+          ],
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 200,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * .9,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 2.0, color: const Color(0x55000000)),
+          ),
+        ),
+        Expanded(
+          child: SizedBox(
+            width: 1,
+            height: 1,
+          ),
+        ),
       ],
     );
   }
